83d88cf70cabc3c63745f31299a929a4d218bc09
Gabriel A P Andrade <gabriel.ap.andraade@gmail.com>
2025-06-09 14:26:09 -0300

    Delete pizzaria directory

--- pizzaria/system/src/utils/validators.ts
+++
@@ -1,8 +0,0 @@
-export function isValidCep(cep: string): boolean {
-  const cleanCep = cep.replace(/\D/g, "");
-  return /^\d{8}$/.test(cleanCep);
-}
-
-export function isEmail(email: string): boolean {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
